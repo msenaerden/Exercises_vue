1eb43ac7db78622d2a2862fdf3f62257d545e2a1
Melike Sena Erden <melikesenaerden@gmail.com>
2024-01-10 21:45:39 +0300

    modified vue

--- project-1/app.js
+++ project-1/app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "Finish the course and learn the Vue",
+      courseGoalA: "Finish the course and learn the Vue",
+      courseGoalB: "You learned Vue",
       vueLink: "https://vuejs.org/",
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
   },
 });
 
