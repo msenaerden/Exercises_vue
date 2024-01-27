43b6427e1a96f49aea616b9fce29afe9e3341132
Melike Sena Erden <melikesenaerden@gmail.com>
2024-01-27 21:04:41 +0300

    modified health bar

--- project-6/app.js
+++ project-6/app.js
@@ -1,9 +1,37 @@
+function randomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
   },
 
-  methods: {},
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+  },
+
+  methods: {
+    attackMonster() {
+      const attackValue = randomValue(5, 12);
+      this.monsterHealth = this.monsterHealth - attackValue;
+      this.attackPlayer();
+    },
+
+    attackPlayer() {
+      const attackValue = randomValue(8, 15);
+      this.playerHealth = this.monsterHealth - attackValue;
+    },
+  },
 });
 
-app.mount("#user-goals");
+app.mount("#game");
